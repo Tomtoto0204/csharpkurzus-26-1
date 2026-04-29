55acf9895ee9ea806dc02ac78513c2268c89f954
Tamás Gér <tomtoto0204@gmail.com>
2026-04-29 16:07:26 +0200

    Játék indítása done

---
+++ U6HXL4/Darts.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+using Darts.Core;
+
+namespace Darts.Client;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        string playerName;
+        int startingPoint;
+        bool doubleOut;
+        bool doubleIn;
+
+
+        Console.WriteLine("Welcome to my Darts application!");
+        Console.Write("> ");
+
+        Console.WriteLine("Player name: ");
+        playerName = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine("Starting point? (301, 501): ");
+        startingPoint = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Double in? [Y/N]");
+        if ((Console.ReadLine() ?? string.Empty) == "Y")
+        {
+            doubleIn = true;
+        }
+        else
+        {
+            doubleIn = false;
+        }
+        Console.WriteLine("Double Out? [Y/N]: ");
+        if ((Console.ReadLine() ?? string.Empty) == "Y")
+        {
+            doubleOut = true;
+        }
+        else
+        {
+            doubleOut = false;
+        }
+
+        Game game = new Game(playerName, startingPoint, doubleIn, doubleOut);
+
+
+        throw new UnreachableException("This shouldn't happen");
+    }
+}
---
+++ U6HXL4/Darts.Core/Game.cs
@@ -0,0 +1,31 @@
+﻿namespace Darts.Core;
+
+public class Game
+{
+    string playerName { get; set; }
+    int startingPoint { get; set; }
+    bool doubleIn { get; set; }
+    bool doubleOut { get; set; }
+
+    public Game(string playerName, int startingPoint, bool doubleIn, bool doubleOut)
+    {
+        this.playerName = playerName;
+        this.startingPoint = startingPoint;
+        this.doubleIn = doubleIn;
+        this.doubleOut = doubleOut;
+    }
+
+
+
+    internal void StartGame(string playerName, int startingPoint, bool doubleIn, bool doubleOut)
+    {
+        this.playerName = playerName;
+        this.startingPoint = startingPoint;
+        this.doubleIn = doubleIn;
+        this.doubleOut = doubleOut;
+    }
+
+
+
+
+}
